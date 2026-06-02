202ebaae85ccb948cae0e81157a7f581375653fe
Iran Situmorang <iran.situmorang@iconpln.co.id>
2026-06-02 23:34:20 +0700

    docker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 1000, // Ngeceknya dilonggarin jadi tiap 1 detik sekali (bawaannya 100ms)
+      ignored: ['**/node_modules/**', '**/.git/**'], // Jangan cek folder-folder ini!
+    },
+  },
+  preview: {
+    port: 3001,
+  },  
+});
